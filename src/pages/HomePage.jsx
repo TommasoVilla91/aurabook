9aeca1ddfa76505a7090cda520594c88f6180849
TommasoVilla91 <tommasovilla@yahoo.it>
2025-06-24 18:55:19 +0200

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import Calendar from '../components/Calendar';
+import styles from './HomePage.module.css';
+
+function HomePagae() {
+
+    const [showCalendar, setShowCalendar] = useState(false);
+
+    return (
+        <div className={styles.homePage}>
+            <section className={styles.titles}>
+                <h1>Benvenuto!</h1>
+                <h4>Qui potrai prenotare la tua visita</h4>
+                <p>Premi il pulsande e scegli il giorno in cui vorresti prenotare</p>
+                <div>
+                    <button
+                        className={styles.bookButton}
+                        onClick={() => setShowCalendar(true)}
+                    >
+                        Prenota ora
+                    </button>
+                </div>
+            </section>
+
+            <Calendar 
+                show={showCalendar}
+            />
+        </div>
+    );
+};
+
+export default HomePagae;
